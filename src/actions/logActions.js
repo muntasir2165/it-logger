a09e4b083d33d6f983107103eec5eb264e9c7ff7
Muntasir Biojid <muntasir2165@hotmail.com>
2020-02-16 18:49:34 -0800

    video 86 - update logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types';
 
 // Get logs from server
@@ -49,7 +52,31 @@ export const deleteLog = id => async dispatch => {
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
-}; 
+};
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await res.json();
+
+    dispatch({ type: UPDATE_LOG, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Set current log
+export const setCurrent = log => ({ type: SET_CURRENT, payload: log });
+
+// Clear current log
+export const clearCurrent = () => ({ type: CLEAR_CURRENT });
 
 // Set loading to true
 export const setLoading = () => ({
