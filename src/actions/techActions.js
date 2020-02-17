6d99b439e606bf998d50b05fc784c6e51e82aae5
Muntasir Biojid <muntasir2165@hotmail.com>
2020-02-16 22:52:10 -0800

    video 88 - techs reducer, action & component

---
+++ src/actions/techActions.js
@@ -0,0 +1,26 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR
+} from './types';
+
+// Get techs from server
+export const getTechs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    dispatch({ type: GET_TECHS, payload: data });
+  } catch (err) {
+    dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => ({
+  type: SET_LOADING
+});
